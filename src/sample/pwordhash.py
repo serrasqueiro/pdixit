fd760304c6fd5dafc49a2f91bc429203a3b7d682
Henrique Moreira <h@serrasqueiro.com>
2021-05-15 15:49:48 +0100

    Nice update

--- src/sample/pwordhash.py
+++ src/sample/pwordhash.py
@@ -39,18 +39,48 @@ def run_main(out, err, args):
 
 def dump_nick(out, err, nick):
     """ Dumps file or nick. """
+    opts = {
+        "show-all": True,
+        }
     # if nick is a two-letter, use e.g. ../../results/strict-pt.lst
     if len(nick) == 2 and nick.isalpha():
         fname = os.path.join("..", "..", "results", f"strict-{nick}.lst")
     else:
         fname = nick
-    dump_wordlist(out, err, fname, ALPHABET_NUM)
+    wset = dump_wordlist(out, err, nick, fname, ALPHABET_NUM, opts)
+    queue = wset['queue']
+    where, maxsize = wset['where'], wset['maxsize']
+    for item in queue:
+        hsh, shown = item
+        if not shown:
+            err.write(f"@ {hsh:>4} <empty>\n")
+            continue
+        out.write(f"@ {hsh:>4} {shown}\n")
+    stats = wset['stats-bysize']
+    for size in sorted(stats):
+        print("# Stat:", size, stats[size])
+    err.write(f"Maximum size for hash {where}: {maxsize}\n")
 
 
-def dump_wordlist(out, err, fname, arange, show_all=True) -> bool:
+def dump_wordlist(out, err, nick:str, fname:str, arange:int, opts:dict) -> dict:
     """ Dumps hash for each word in a file. """
     enc = "ISO-8859-1"
+    show_all = bool(opts.get("show-all"))
+    queue, hshing = list(), list()
+    wset = {
+        'queue': queue,
+        'hshing': hshing,
+        'where': -1,
+        'maxsize': -1,
+        'hsh-capital': list(),	# hsh with (at least one) first letter capital
+        'stats-bysize': dict(),
+        }
     lines = open(fname, "r", encoding=enc).readlines()
+    if nick:
+        lst_exc_fname = os.path.join(os.path.dirname(fname), "exc-" + nick + ".lst")
+        excl = from_exclusion_file(lst_exc_fname, enc)
+    else:
+        excl = from_exclusion_file()
     words = [valid_word(word.rstrip('\n')) for word in lines if not word.startswith('#')]
     dct, bysize = dict(), dict()
     for size in range(3, 7+1, 1):
@@ -64,6 +94,8 @@ def dump_wordlist(out, err, fname, arange, show_all=True) -> bool:
         word = char_map.simpler_ascii(aword, 1)
         s_word = char_map.simpler_ascii(aword)
         hsh = word_hash(word)
+        #if hsh != 269:
+        #    continue
         dct[hsh].append((word, s_word))
         last = s_word
         size = len(s_word)
@@ -78,29 +110,52 @@ def dump_wordlist(out, err, fname, arange, show_all=True) -> bool:
     for hsh in range(arange):
         words = dct[hsh]
         shown = ';'.join([word for word, _ in words])
-        out.write(f"@ {hsh:>4} {shown}\n")
-    if not show_all:
-        return True
+        queue.append((hsh, shown))
+    infos, excluded = list(), excl['must']
     maxsize, where = -1, hsh
+    # Stats
+    wset['stats-bysize'][0] = 0
+    for size in range(3, 7+1, 1):
+        wset['stats-bysize'][size] = 0
+    # Main loop
     for hsh in range(arange):
         idx = 0
+        candidates = list()
         for size in range(3, 7+1, 1):
             words = bysize[size][hsh]
             if not words:
                 continue
-            shown = ';'.join([word for word in words])
-            out.write(f"bysize:{size} {hsh:>4} {shown}\n")
+            rest = [word for word in words if word.islower() and not word in excluded]
+            if not rest:
+                continue
+            if len(candidates) >= 3:
+                continue
+            candidates.append((size, hsh, rest))
+        if candidates:
+            size, hsh, rest = candidates[0]
+            hshing.append(candidates[0])
+            wset['stats-bysize'][size] += 1
             idx = size
             if idx > maxsize:
                 maxsize, where = idx, hsh
-            break
         if idx <= 0:
-            out.write(f"bysize:- {hsh:>4} (NADA)\n")
-    err.write(f"Maximum size for hash {where}: {maxsize}\n")
-    return True
+            hshing.append((0, hsh, ["(NADA)"]))
+            #out.write(f"bysize:- {hsh:>4} (NADA)\n")
+            wset['stats-bysize'][0] += 1
+    wset['hsh-capital'] = infos
+    wset['where'], wset['maxsize'] = where, maxsize
+    if not show_all:
+        return wset
+    # Dump if requested:
+    for size, hsh, rest in hshing:
+        shown = ";".join(rest)
+        s_size = "-" if size == 0 else str(size)
+        out.write(f"bysize:{s_size} {hsh:>4} {shown}\n")
+    return wset
 
 
 def valid_word(aword) -> str:
+    """ Validate word, strictly """
     assert aword.isalpha()
     assert aword.strip() == aword
     return aword
@@ -111,6 +166,22 @@ def word_hash(astr) -> int:
     return aval
 
 
+def from_exclusion_file(fname="", encoding="", debug=0) -> dict:
+    """ Returns the dictionary of excluded words. """
+    excl = {
+        "must": dict(),
+        }
+    if not fname:
+        return excl
+    lines = open(fname, "r", encoding=encoding).readlines()
+    words = [line.split("@")[0] for line in lines if not line.startswith("#")]
+    for word in words:
+        for a_chr in word:
+            assert a_chr.isalpha() or ord(a_chr) >= 0xa0, f"Invalid char: {ord(a_chr)}d"
+        excl['must'][word] = True
+    if debug > 0:
+        print("excl:", sorted(excl['must'], key=str.casefold))
+    return excl
 
 
 # Main script
